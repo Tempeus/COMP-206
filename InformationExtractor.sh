13f4ab0e60fad741866fe6dcf266fe48802cb662
Tempeus <36718822+Tempeus@users.noreply.github.com>
2019-01-25 14:13:22 -0500

    Add files via upload

---
+++ InformationExtractor.sh
@@ -0,0 +1,61 @@
+#! bin/bash
+
+#This script will be used to automate the process of earching and filtering information then logging it to a log file
+
+# $1 is the directory's name
+
+#Clearing the screen
+clear
+
+#change directory to home directory
+cd ~
+
+#verifies whether a subdirectory called #1 exists, if it does not exist it will create the directory
+File=$1
+
+if [$File]
+then 
+	echo "File $File exists."
+else
+	mkdir $1
+fi
+
+#Moving into $1
+cd $1
+
+#Verifies if extractor.log exists, if not ,it wil lcreate a log file
+if [extractor.log]
+then
+	echo "extractor.log exists"
+else
+	touch extractor.log
+fi
+
+#verify if file called information exists, it not, it will create the file
+if [Information]
+then
+	echo "Information file already exists"
+else
+	touch Information
+	echo "Information file is created"
+fi
+
+#Changing the security of the Information file
+chmod 540 Information
+
+#Writting into the log
+echo "Access right changed for Information file" >> extractor.log
+
+#Appending users into Information file
+Users=who|grep"[a-d]"
+Num=who|grep -c "[a-d]"
+
+#Notifying the change log
+
+
+#Notifying number of found users into log file
+
+
+#Termination of script
+echo "Execution of InformationExtractor script has ended successfully!"
+
